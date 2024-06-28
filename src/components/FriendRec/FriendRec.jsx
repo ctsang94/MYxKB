7513831c4552f0bb6f2ed88ac6cf7d32e0a3b2e7
Kelvin <ctsang94@gmail.com>
2024-06-28 01:17:35 -0400

    Modal Completion

--- src/components/FriendRec/FriendRec.jsx
+++ src/components/FriendRec/FriendRec.jsx
@@ -4,14 +4,14 @@ import image from '../../assets/google.jpeg';
 
 const FriendRec = () => {
   return (
-    <div className="container">
-      <div className="container__text-box">
-        <div className="container__title">Friend-Recommended Results</div>
-        <div className="container__description">
+    <div className="section">
+      <div className="section__text-box">
+        <div className="section__title">Friend-Recommended Results</div>
+        <div className="section__description">
         Allow location sharing and AI will restaurants your friends have visited and loved. Discover new brunch spots or hidden gems for dinner based on their reviews, photos, and ratings. Turn dining out into a shared adventure with Friend-Recommended Restaurants.        </div>
       </div>
       
-      <img className="container__image" src={image} alt="Placeholder" />
+      <img className="section__image" src={image} alt="Placeholder" />
     </div>
   );
 };
